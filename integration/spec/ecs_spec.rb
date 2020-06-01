6c3c9c7d1a6185e422ec6cba66fe17727a6886db
chris-malloy <chrismalloymusic@gmail.com>
2020-06-01 15:56:58 -0400

    Testing ecs task.

--- integration/spec/ecs_spec.rb
+++ integration/spec/ecs_spec.rb
@@ -15,7 +15,24 @@ describe ecs_service("#{TFVARS['app_name']}-svc"), cluster: TFVARS['cluster_name
   end
 
   its(:desired_count) { should eq TFVARS['desired_task_count']}
+  its(:running_count) { should eq TFVARS['desired_task_count']}
   its(:launch_type) { should eq TFVARS['launch_type']}
   its(:task_definition) { should eq ENVVARS[:task_definition_arn][:value]}
   its(:status) { should eq 'ACTIVE'}
+end
+
+describe ecs_task_definition(ENVVARS[:task_definition_arn][:value]) do
+  it { should exist }
+  it { should be_active }
+  its(:task_role_arn) { should eq TFVARS['task_role_arn']}
+
+  it 'should have the correct volumes' do
+    volumes = subject.volumes.map {|volume| volume}
+
+    expect(volumes[0].name).to eq TFVARS['volumes'][0]['name']
+    expect(volumes[0].host['source_path']).to eq TFVARS['volumes'][0]['host_path']
+  end
+
+  its(:cpu) { should eq TFVARS['cpu']}
+  its(:memory) { should eq TFVARS['memory']}
 end
